59e2486e8eb516401d04dfea94a61e219c8fd6ce
Patrickgp <86730331+Patrickgp@users.noreply.github.com>
2021-10-22 22:06:07 -0400

    practice

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+
+
 @app.get('/')
 def home():
-    return "Did it!"
+    return "did it!"
+
+inventory = {
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None, description="The ID of the item you would like to view.")):
+    return inventory[item_id]
+
+@app.get("/get-by-name/")
+def get_item(*, name: Optional[str] = None, test: Optional[int]):
+    for item_id in inventory:
+        if inventory[item_id].name == name:
+            return inventory[item_id]
+    return{"Data": "Not found"}
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists."}
+
+    inventory[item_id] = item
+    return inventory[item_id]
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        return {"Error": "Item ID does not exists."}
+
+    if item.name != None:
+        inventory[item_id].name = item.name
+
+    if item.price != None:
+        inventory[item_id].price = item.price
+
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+
+    return inventory[item_id]
